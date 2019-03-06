6817e4d3620e2cd59489bc410a0b4687984b6425
mercykwazi <you@example.com>
2019-03-06 10:52:11 +0200

    added two practice files for java

---
+++ testing-java/src/Main.java
@@ -0,0 +1,19 @@
+
+import Practice
+public class Main {
+   // String name ="cindy";
+public static void main(String[] args){
+
+
+    int x = 5;
+    int y = 6;
+  //  System.out.println(name);
+
+    Practice firstCar = new Practice.practice(4 , 5);
+
+    System.out.println(firstCar.getSeats());
+
+}
+
+
+}
---
+++ testing-java/src/Practice.java
@@ -0,0 +1,20 @@
+public class Practice{
+
+    private int wheels;
+    private int seats;
+
+
+    public void practice(int wheels, int seats) {
+        this.wheels = wheels;
+        this.seats = seats;
+
+    }
+
+    public int getSeats(){
+
+        return seats;
+    }
+
+
+
+}
