8a8facf4a0911d76118921cc46ec3d1e0a6a25bb
mercykwazi <you@example.com>
2019-05-06 16:13:50 +0200

    added a new folder

---
+++ dependencyInjection/src/main/java/User.java
@@ -0,0 +1,24 @@
+public class User {
+private String lastName;
+private String firstName;
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public static void main(String[] args){
+
+    }
+}
