1fe2855092a150c450248447e00eaefaa719f347
mercykwazi <you@example.com>
2019-06-20 08:27:06 +0200

    added a new app that uses the java

---
+++ storageApp/src/main/java/Database.java
@@ -0,0 +1,29 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+public class Database{
+    private final String url = "jdbc:postgresql://localhost/storage";
+    private final String user = "postgres";
+    private final String password = "Gugulethu";
+
+    public Connection connect() {
+        Connection conn = null;
+        Statement stmt = null;
+        try {
+            Class.forName("org.postgresql.Driver");
+            conn = DriverManager.getConnection(url, user, password);
+            System.out.println("Connected to the PostgreSQL server successfully.");
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
+}
