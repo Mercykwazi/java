1119af08a01f22642ac1e721efbc9592a3a27291
mercykwazi <you@example.com>
2019-06-20 16:11:09 +0200

    used the DatasourceConfig file to config my database

--- storageApp/src/main/java/com/mercy/storageApp/Database.java
+++ storageApp/src/main/java/com/mercy/storageApp/Database.java
@@ -9,16 +9,10 @@ import java.sql.Statement;
 @Component
 public class Database {
     DatasourceConfig config=new DatasourceConfig();
-
-//    private final String url = "jdbc:postgresql://localhost/storage";
-//    private final String user = "postgres";
-//    private final String password = "Gugulethu";
-
     public Connection connect() {
         Connection conn = null;
         Statement stmt = null;
-        //            Class.forName("org.postgresql.Driver");
-//            conn = DriverManager.getConnection(url, user, password);
+
         config.datasource();
         System.out.println("Connected to the PostgreSQL server successfully.");
 
